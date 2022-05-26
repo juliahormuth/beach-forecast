b6d9bfb8a4807afb3fb320a29ff714c5be070941
Julia Hormuth <jvhormuth@gmail.com>
2022-05-26 08:29:15 -0300

    setup tests

---
+++ test/jest-setup.ts
@@ -0,0 +1,9 @@
+import { SetupServer } from '@src/server'
+import supertest from 'supertest'
+
+let server: SetupServer
+beforeAll(() => {
+    server = new SetupServer()
+    server.init()
+    global.testRequest = supertest(server.getApp())
+})
